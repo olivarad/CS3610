17ff859ec0d1540941602669f086b206ff728d2e
root <root@Olivias-PC>
2023-11-21 14:40:32 -0500

    Project05: Working on generalizing dijkstra's algorithm to fix bugs

--- Projects/Project05/main.cc
+++ Projects/Project05/main.cc
@@ -46,68 +46,49 @@ int minDist(int dist[], bool included[], int n){
     return min_index;
 }
 
-bool visit(int i, vector<int> visited){
-    for (unsigned int j = 0; j < visited.size(); j++){
-        if (i == visited[j]){ // i has been visited
-            return true;
+bool no_visit(int n, string city, string unvisited[]){
+    for (int i = 0; i < n; i++)
+        if (city == unvisited[i])
+            return true; // City has not been visited
+    
+    return false; // City has been visited
+}
+
+int shortest(int n, pair<string, pair<int, string>> table[], string unvisited[]){
+    int initial;
+    int shortest;
+    for (int i = 0; i < n; i++)
+        if (unvisited[i] != "NULL"){
+            initial = i;
+            shortest = table[i].second.first;
+            break;
         }
-    }
-    return false;
+    
+    for (int i = initial; i< n; i++)
+        if (table[i].second.first < shortest && no_visit(n, table[i].first, unvisited))
+            shortest = table[i].second.first;
+    
+    return shortest;
 }
 
 void dijkstra(int n, vector<vector<int>> matrix, string cities[]){
-    vector<int> visited;
-    vector<int>unvisited(n);
-    for (int i = 0; i < n; i++)
-        unvisited[n] = i;
-    int shortest_dist[n] = {0, 999}; // 0: same city, 999: no shortest path found
-    int previous_index[n] = {999};
+    typedef pair<string, pair<int, string>> Data; // City (.first), Shortest distance from A (.second.first), Previous city (.second.second)
+    Data table[n]; // Array to store data relevant to dijkstras algorithm
+
+    string visited[n] = {"NULL"}; // Initialized to null to indicate that no cities have been visited
+    string unvisited[n]; // Unvisited cities
     
+    table[0].first = cities[0];
+    table[0].second.first = 0;
+    table[0].second.second = "NULL"; // No previous city
+
+    unvisited[0] = cities[0]; // Initialized seperately to simply for loop in the setup
     for (int i = 1; i < n; i++){
-        if (matrix[0][i] != 0){ // Path exists
-            if(matrix[0][0] + matrix[0][i] < shortest_dist[i]){ // Update shortest know distance
-                shortest_dist[i] = matrix[0][0] + matrix[0][i];
-                previous_index[i] = 0; // Visited city i by traveling from city 0
-            }
-        }
-        visited.push_back(0); // Add city 0 to list of visited cities
-        unvisited.erase(unvisited.begin()); // Erase the city just visited from unvisited
+        table[i].first = cities[i];
+        table[i].second.first = 999; // Indicates no known shortest distance
+        table[i].second.second = "NULL"; // No previous city
+        unvisited[i] = cities[i];
     }
 
-    while (!unvisited.empty()){
-        int smallest_dist = 999;
-        int city; // City now traveling from
-        for (unsigned int i = 0; i < unvisited.size(); i++)
-            if (shortest_dist[i] != 0 && shortest_dist[i] < smallest_dist){
-                smallest_dist = shortest_dist[i];
-                city = i;
-            }
-        
-        for (unsigned int i = 0; i < unvisited.size(); i++){
-            if (matrix[city][i] != 0 && !visit(i, visited)){ // Path exists and has not been visited yet
-                if (shortest_dist[city] + matrix[city][i] < shortest_dist[i]){ // Found better path to city i
-                    shortest_dist[i] = shortest_dist[city] + matrix[city][i];
-                    previous_index[i] = city;
-                }
-            }
-            visited.push_back(city);
-            for (vector<int>::iterator it = unvisited.begin(); it != unvisited.end(); ++it){
-                if (*it == city){
-                    unvisited.erase(it);
-                    break;
-                }
-            }
-        }
-    }
 
-    vector<string> results;
-    results.insert(results.begin(), cities[n]);
-    int prev = previous_index[n];
-    while (prev != 0){
-        results.insert(results.begin(), cities[prev]);
-    }
-    for (unsigned int i = 0; i < results.size(); i++){
-        cout << results[i] << " ";
-    }
-    cout << endl;
 }
